cdcdcffc8cc060d30cb0cdde8fff860daa727760
Aicidia <83481767+Aicidia@users.noreply.github.com>
2021-04-30 20:58:12 -0400

    Create randomly generated platformer.py

---
+++ randomly generated platformer.py
@@ -0,0 +1,28 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+import random as rng
+
+app=Ursina()
+
+a=0
+
+def plat(d,o):
+    global a
+    a+=d
+    Entity(model='cube',color=color.cyan,position=(o,-50,a),collider='box',scale=(5,100,10),texture='vertical_gradient')
+
+def randplat():
+    plat(rng.randint(17,20),rng.randint(-5,5))
+
+plat(0,0)
+
+Entity(model='cube',color=color.black,position=(0,-50,0),collider='box',scale=(10000,1,10000),texture='circle')
+
+numplat=input('# of platforms\n')
+
+for i in range(1,int(numplat)):
+    randplat()
+
+player=FirstPersonController(speed=20)
+
+app.run()
